1560c4149ef1c0f166e9a903da9ccdb7f7d97b77
Aidan Jaramillo <aidanjaramillo0326@gmail.com>
2026-02-17 09:27:53 -0600

    added basic login

--- assets/js/modules/auth.js
+++ assets/js/modules/auth.js
@@ -1,4 +1,4 @@
-const registerForm = document.getElementById("registerForm");
+const registerForm = document.getElementById("registerForm"); //registration
 
 if (registerForm){
     registerForm.addEventListener("submit", async(e) => {
@@ -13,5 +13,22 @@ if (registerForm){
         localStorage.setItem("user", JSON.stringify(user));
         alert("Registration successful! Please log in.");
         window.location.href = "login.html";
+    })
+}
+
+const loginForm = document.getElementById("loginForm"); //login
+
+if(loginForm){
+    loginForm.addEventListener("submit", async(e) => {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+
+        if(storedUser && storedUser.email === email && storedUser.password === password){
+            console.log("Login successful!");
+            window.location.href = "dashboard.html";
+        }
     })
 }
